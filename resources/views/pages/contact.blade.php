5b98f58485015eacfb1fb442372109b4faae1e45
Madeline Griep <mdg32@njit.edu>
2019-03-15 09:15:52 -0400

    Task: create pages directory with files about contact home projects

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: madelinegriep
+ * Date: 3/15/19
+ * Time: 8:52 AM
+ */
+@extends('layouts.default')
+@section('content')
+    i am the contact page
+@stop
---
+++ resources/views/pages/projects.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: madelinegriep
+ * Date: 3/15/19
+ * Time: 8:52 AM
+ */
