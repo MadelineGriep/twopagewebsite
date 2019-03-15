4abb68ec706ec532d1c40672ea9ab6823fae6917
Madeline Griep <mdg32@njit.edu>
2019-03-15 09:26:26 -0400

    Task: create sidebar layout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,11 +5,36 @@
  * Date: 3/15/19
  * Time: 8:51 AM
  */
-<!-- sidebar nav -->
-    <nav id="sidebar-nav">
-        <ul class="nav nav-pills nav-stacked">
-            <li><a href="#">Fly to the Moon</a></li>
-            <li><a href="#">Dig to China</a></li>
-            <li><a href="#">Swim Across the Sea</a></li>
-        </ul>
-    </nav>
+<!doctype html>
+<html>
+<head>
+@include('includes.head')
+</head>
+<body>
+<div class="container">
+
+    <header class="row">
+        @include('includes.header')
+    </header>
+
+    <div id="main" class="row">
+
+        <!-- sidebar content -->
+        <div id="sidebar" class="col-md-4">
+            @include('includes.sidebar')
+        </div>
+
+        <!-- main content -->
+        <div id="content" class="col-md-8">
+            @yield('content')
+        </div>
+
+    </div>
+
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
+
+</div>
+</body>
+</html>
