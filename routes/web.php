46ec4315e03535695b5580ef9649fb029b860da8
Madeline Griep <mdg32@njit.edu>
2019-03-15 09:17:29 -0400

    Task: set up route

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function()
+{
+    return View::make('pages.home');
+});
+Route::get('about', function()
+{
+    return View::make('pages.about');
+});
+Route::get('projects', function()
+{
+    return View::make('pages.projects');
+});
+Route::get('contact', function()
+{
+    return View::make('pages.contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
