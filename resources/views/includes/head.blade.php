d7543fbcb887880a7ca2e0178eabbba33ae6a3d2
Madeline Griep <mdg32@njit.edu>
2019-03-23 10:07:55 -0400

    Task: change head blade

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -10,7 +10,7 @@
 <meta name="description" content="">
 <meta name="author" content="Scotch">
 
-<title>Super Cool Layouts</title>
+<title>Maddy's First Laravel Webpage</title>
 
 <!-- load bootstrap from a cdn -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
