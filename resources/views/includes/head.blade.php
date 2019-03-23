9f0b868138309d99f1f757b6c3ca666073c81062
Madeline Griep <mdg32@njit.edu>
2019-03-23 10:02:59 -0400

    Task: update header and footer

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,6 +5,7 @@
  * Date: 3/15/19
  * Time: 8:53 AM
  */
+?>
 <meta charset="utf-8">
 <meta name="description" content="">
 <meta name="author" content="Scotch">
@@ -12,4 +13,5 @@
 <title>Super Cool Layouts</title>
 
 <!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
